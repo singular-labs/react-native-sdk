e45c7c82f353235abd4d68939dc2d5d7b211214d
tapasyat <tapasya@singular.net>
2021-05-27 14:53:23 +0530

    cr changes

--- SingularAdData.js
+++ SingularAdData.js
@@ -32,50 +32,62 @@ export class SingularAdData {
 
     withNetworkName(networkName) {
         this[ADMON_NETWORK_NAME] = networkName;
+        return this;
     }
 
     withAdType(adType) {
         this[ADMON_AD_TYPE] = adType;
+        return this;
     }
 
     withGroupType(adGroupType) {
         this[ADMON_AD_GROUP_TYPE] = adGroupType;
+        return this;
     }
 
     withImpressionId(impressionId) {
         this[ADMON_IMPRESSION_ID] = impressionId;
+        return this;
     }
 
     withAdPlacementName(adPlacementName) {
         this[ADMON_AD_PLACEMENT_NAME] = adPlacementName;
+        return this;
     }
 
     withAdUnitId(adUnitId) {
         this[ADMON_AD_UNIT_ID] = adUnitId;
+        return this;
     }
 
     withAdGroupId(adGroupId) {
         this[ADMON_AD_GROUP_ID] = adGroupId;
+        return this;
     }
 
     withAdGroupName(adGroupName) {
         this[ADMON_AD_GROUP_NAME] = adGroupName;
+        return this;
     }
 
     withAdGroupPriority(adGroupPriority) {
         this[ADMON_AD_GROUP_PRIORITY] = adGroupPriority;
+        return this;
     }
 
     withPrecision(precision) {
         this[ADMON_PRECISION] = precision;
+        return this;
     }
 
     withPlacementId(placementId) {
         this[ADMON_PLACEMENT_ID] = placementId;
+        return this;
     }
 
     withAdUnitName(adUnitName) {
         this[ADMON_AD_UNIT_NAME] = adUnitName;
+        return this;
     }
 
     hasRequiredParams() {
